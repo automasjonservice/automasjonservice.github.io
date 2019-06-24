f833bf07da1742b6204b0af42b867c88013927df
Jørund Martinsen <jorund@automasjonservice.no>
2019-06-24 23:14:13 +0200

    Updates

---
+++ precache-manifest.370fa2de3bf3d59d6108e1733301fc6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ec0f91d2296c174c07fffe002ef3a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef4e5c95fa4bebeb97e2",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ef4e5c95fa4bebeb97e2",
+    "url": "/static/js/main.cd40451c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
